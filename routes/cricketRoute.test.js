b1a12630e25430cefb2a4dfdda4101100137d939
Nabeel <zafarnabeel2@gmail.com>
2023-05-17 12:11:55 +0500

    Pass Test

--- routes/cricketRoute.test.js
+++ routes/cricketRoute.test.js
@@ -3,5 +3,5 @@ const check = require("./cricketRoute")
 const testResponse = require('./cricketRoute');
 
 test("I am batman", () => {
-    expect('batman').toMatch('mad');
+    expect('batman').toMatch('man');
   });
